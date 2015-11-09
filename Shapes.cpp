4a038439daecbacf866ff91d2232f887c6028430
PeymanT <fedora.toby@gmail.com>
2015-11-09 11:56:37 +0100

    Sphere raytracing (no shading)

--- Shapes.cpp
+++ Shapes.cpp
@@ -23,12 +23,12 @@ void PPlane::test(Ray& ray, HitData& hit)
 	{
 		t = temp / tempDivisor; //Calculate the t value
 
-		if (hit.t == -1 && t >= 0) //Initial value stored in hit.t if t is larger than 0
+		if (hit.t == -1 && t > 0) //Initial value stored in hit.t if t is larger than 0
 		{
 			hit.t = t;
 			hit.lastShape = this;
 		}
-		else if (t < hit.t && t >= 0) //t stored if smaller than hit.t and greater than zero.
+		else if (t < hit.t && t > 0) //t stored if smaller than hit.t and greater than zero.
 		{
 			hit.t = t;
 			hit.lastShape = this;
@@ -58,21 +58,51 @@ PSphere::PSphere(Vec _center, float _radius, Color _color)
 
 void PSphere::test(Ray& ray, HitData& hit)
 {
-	float t1, t2;
+	float min_t;
+	float t[2];
 	float b = ray.d.Dot(ray.o - center);
 	float w = ((ray.o - center).Dot(ray.o - center)) - pow(radius, 2);
-	t1 = -b + sqrt(pow(b, 2) - w);
-	t2 = -b - sqrt(pow(b, 2) - w);
+	t[0] = -b + sqrt(pow(b, 2) - w);
+	t[1] = -b - sqrt(pow(b, 2) - w);
 	
-	if (pow(b, 2) - w > 0)
+	//Find which t is the smallest value
+	if (t[0] < t[1])
 	{
-		hit.lastShape = this;
+		min_t = t[0];
+	}
+	else if (t[0] > t[1])
+	{
+		min_t = t[1];
+	}
+
+	//Determine if hit
+	if (hit.t == -1 && min_t > 0)
+	{
+		hit.t = min_t;
+
+		if (pow(b, 2) - w > 0) //Only store shape if b^2 - w is greater than zero
+		{
+			hit.lastShape = this;
+		}
+	}
+	else if (min_t < hit.t && min_t > 0)
+	{
+		hit.t = min_t;
+
+		if (pow(b, 2) - w > 0)
+		{
+			hit.lastShape = this;
+		}
 	}
 }
-
+//vvvvvvv NOT DONE vvvvvvv
 Vec PSphere::normal(Vec& point)
 {
-	return center;
+	//Find vector from center to point on surface and normalize it
+	Vec a  = point - center;
+	a.Normalize();
+
+	return a;
 }
 //---------------------------------------------------------------------
 
